3a3267043531e6c113dd3ee6a85c4ec5582f336c
PCapid3v <velenum@gmail.com>
2022-03-24 10:22:35 +0000

    committ trying to fix images

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,5 +1,6 @@
 import React from "react"
-
+import star from "../images/star.png"
+import pictures from "../images/katie-zaferes.png"
 export default function Card(props) {
     let badgeText
     if (props.openSpots === 0) {
@@ -7,19 +8,19 @@ export default function Card(props) {
     } else if (props.location === "Online") {
         badgeText = "ONLINE"
     }
-    
+    //need to fix the images
     return (
         <div className="card">
             {
                 badgeText && 
                 <div className="card--badge">{badgeText}</div>
-            }
+            }            
             <img 
-                src={`../images/${props.coverImg}`} 
+                src={pictures}
                 className="card--image" 
             />
             <div className="card--stats">
-                <img src="../images/star.png" className="card--star" />
+                <img src={star} className="card--star"  />
                 <span>{props.stats.rating}</span>
                 <span className="gray">({props.stats.reviewCount}) • </span>
                 <span className="gray">{props.location}</span>
